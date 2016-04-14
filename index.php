c02399dcddaf4ea9653cdfc53b4334e2729b1cee
qanthoine <qanthoine@gmail.com>
2016-04-14 14:55:44 +0200

    Code dans index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Quiz</title>
+    </head>
+
+    <body>
+    	<center>
+    		<h1>Mes Quiz</h1>
+    	</center>
+    	<h2>Les Maths</h2>
+    	Un Quiz sur les maths ! Les additions, multiplications, soustraction, ..<br><br>
+    	<a href="quiz/maths.php">Lancer le Quiz !</a>
+    	<h2>Les Animaux</h2>
+    	Un Quiz sur les animaux ! Les chiens, chats, lapins, ..<br><br>
+    	<a href="quiz/animaux.php">Lancer le Quiz !</a>
+    	<h2>Les Montagnes</h2>
+    	Un Quiz sur les montagnes ! Les Alpes, le Mont-Blanc, les Voges, ..<br><br>
+    	<a href="quiz/montagnes.php">Lancer le Quiz !</a>
+
+
+    </body>
+</html>
