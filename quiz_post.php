fbd2673dccbad04d8fd0ccbcfd4fe65b2c980bca
qanthoine <qanthoine@gmail.com>
2016-04-17 20:21:21 +0200

    Ajout + code quiz_post.php pour gestion formulaire

---
+++ quiz_post.php
@@ -0,0 +1,24 @@
+<?php
+include('bdd.php');
+if(!empty($_POST['id_quiz']))
+{
+	$id_quiz = htmlspecialchars($_POST['id_quiz']);	
+	$req = $bdd->query('SELECT * FROM quiz_questions WHERE quiz_id = '.$id_quiz.'');
+	$nb_q = $req->rowcount();
+	if(count($_POST['input']) == $nb_q)
+	{
+		
+		for($i=1;$i<=$nb_q;$i++)
+		{
+			echo "Bonjour";		
+		}
+	}
+	else
+	{
+		header('Location: quiz.php?quiz='.$id_quiz);
+	}
+}
+else
+{
+	header('Location: quiz.php?quiz='.$id_quiz);
+}
