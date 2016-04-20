ac8a55405cdbee9e491a845ce62b75ffcd8441f4
qanthoine <qanthoine@gmail.com>
2016-04-20 15:32:25 +0200

    Simplification de la configuration

--- bdd.php
+++ bdd.php
@@ -1,3 +1,16 @@
 <?php
-$bdd = new PDO('mysql:host=localhost;dbname=site;charset=utf8', 'root', '');
+// Adresse du serveur MYSQL :
+$adresse = '127.0.0.1';
+//////////////////////////////////
+// Nom de la DataBase :
+$db = 'site';
+//////////////////////////////////
+// Nom d'Utilisateur :
+$utilisateur = 'root';
+//////////////////////////////////
+// Mot de Passe de l'Utilisateur :
+$pass = '';
+//////////////////////////////////
+// Variable $bdd :
+$bdd = new PDO('mysql:host='.$adresse.';dbname='.$db.';charset=utf8', $utilisateur, $pass);
 ?>
