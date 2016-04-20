1dbefcd42f1c8a26e3032e293040e9cda139a55b
qanthoine <qanthoine@gmail.com>
2016-04-20 02:15:28 +0200

    Modification de la colonne quiz en id_quiz

--- sql/quiz_reponses_bdd.sql
+++ sql/quiz_reponses_bdd.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50709
 File Encoding         : 65001
 
-Date: 2016-04-14 23:45:22
+Date: 2016-04-20 02:14:38
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -21,26 +21,27 @@ SET FOREIGN_KEY_CHECKS=0;
 DROP TABLE IF EXISTS `quiz_reponses`;
 CREATE TABLE `quiz_reponses` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `quiz` int(11) NOT NULL,
+  `quiz_id` int(11) NOT NULL,
   `id_question` int(11) NOT NULL,
   `id_reponse` int(11) NOT NULL,
   `reponse` varchar(255) NOT NULL,
+  `resultat` tinyint(4) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
 
 -- ----------------------------
 -- Records of quiz_reponses
 -- ----------------------------
-INSERT INTO `quiz_reponses` VALUES ('1', '1', '1', '1', '2');
-INSERT INTO `quiz_reponses` VALUES ('2', '1', '1', '2', '4');
-INSERT INTO `quiz_reponses` VALUES ('3', '1', '1', '3', '6');
-INSERT INTO `quiz_reponses` VALUES ('4', '1', '2', '1', '132');
-INSERT INTO `quiz_reponses` VALUES ('5', '1', '2', '2', '180');
-INSERT INTO `quiz_reponses` VALUES ('6', '1', '2', '3', '453');
-INSERT INTO `quiz_reponses` VALUES ('7', '1', '3', '1', '12.5');
-INSERT INTO `quiz_reponses` VALUES ('8', '1', '3', '2', '3.54');
-INSERT INTO `quiz_reponses` VALUES ('9', '1', '3', '3', '5.4');
-INSERT INTO `quiz_reponses` VALUES ('10', '1', '3', '4', '6.7');
-INSERT INTO `quiz_reponses` VALUES ('11', '1', '3', '5', '5.9');
-INSERT INTO `quiz_reponses` VALUES ('12', '1', '4', '1', '4');
-INSERT INTO `quiz_reponses` VALUES ('13', '1', '4', '2', '5');
+INSERT INTO `quiz_reponses` VALUES ('1', '1', '1', '1', '2', '1');
+INSERT INTO `quiz_reponses` VALUES ('2', '1', '1', '2', '4', '0');
+INSERT INTO `quiz_reponses` VALUES ('3', '1', '1', '3', '6', '0');
+INSERT INTO `quiz_reponses` VALUES ('4', '1', '2', '1', '132', '0');
+INSERT INTO `quiz_reponses` VALUES ('5', '1', '2', '2', '180', '1');
+INSERT INTO `quiz_reponses` VALUES ('6', '1', '2', '3', '453', '0');
+INSERT INTO `quiz_reponses` VALUES ('7', '1', '3', '1', '12.5', '0');
+INSERT INTO `quiz_reponses` VALUES ('8', '1', '3', '2', '3.54', '0');
+INSERT INTO `quiz_reponses` VALUES ('9', '1', '3', '3', '5.4', '1');
+INSERT INTO `quiz_reponses` VALUES ('10', '1', '3', '4', '6.7', '0');
+INSERT INTO `quiz_reponses` VALUES ('11', '1', '3', '5', '5.9', '0');
+INSERT INTO `quiz_reponses` VALUES ('12', '1', '4', '1', '4', '1');
+INSERT INTO `quiz_reponses` VALUES ('13', '1', '4', '2', '5', '0');
